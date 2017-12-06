dc70d828dc87cdddc0aa5bbac15f78b925755fbd
zhouniyao <zhouniyao@163.com>
2017-12-06 13:55:18 +0800

    niming

---
+++ NanGongMeng_Tutor/Assets/Script/Launcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour {
+	public GameObject ballPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(1)){//点击右键
+			Instantiate(this.ballPrefab);
+		}
+	}
+}
---
+++ NanGongMeng_Tutor/Assets/Script/Shoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = new Vector3 (-6.0f, 10.0f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnBecameInvisible(){
+		Destroy (this.gameObject);
+	}
+	
+}
